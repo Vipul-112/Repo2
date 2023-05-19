6d2c1c9ab341be9bb2abc81111bf24b183d048e0
Vipul-112 <whistler20466289@gmail.com>
2023-05-19 09:42:42 +0530

    modified c3

--- src/Git/C3.java
+++ src/Git/C3.java
@@ -4,7 +4,8 @@ public class C3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("new class");
+		System.out.println("new class-- make a new changes");
+		System.out.println("new code here");
 
 	}
 
